b6b6cc5e054402bb2e6d65782e29615128192815
Twsha Vyass <88026581+TwiVyass@users.noreply.github.com>
2023-09-09 20:16:13 +0530

    Qno4.java

--- Qno4.java
+++ Qno4.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 class Spy
 {
-    public static void main(String[] args) 
+    public static void main(String[] args)
   {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number: ");
