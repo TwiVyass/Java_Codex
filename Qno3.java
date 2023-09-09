0e662b94323622b1cd7a65ff06d9962442794635
Twsha Vyass <88026581+TwiVyass@users.noreply.github.com>
2023-09-09 20:14:17 +0530

    Qno3.java

--- Qno3.java
+++ Qno3.java
@@ -5,19 +5,19 @@ class SumOfDigits
     public static void main(String[] args) 
   {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.println("Enter a number: ");
         int num = scanner.nextInt();
 
         int sum = 0;
-        int originalNum = num;
+        int n = num;
 
         while (num > 0) 
         {
             int digit = num % 10;
-            sum += digit;
-            num /= 10;
+            sum = sum + digit;
+            num = num/10;
         }
 
-        System.out.println("Sum of digits of " + originalNum + " is " + sum);
+        System.out.println("Sum of digits of " + n + " is " + sum);
     }
 }
