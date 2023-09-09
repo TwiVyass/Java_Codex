f6bdf2f1ab5391f33dd7ecfc065f75cac3772aa3
Twsha Vyass <88026581+TwiVyass@users.noreply.github.com>
2023-09-09 19:22:34 +0530

    Create 3.java

---
+++ 3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Spy
+{
+    public static void main(String[] args) 
+  {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = scanner.nextInt();
+
+        int n = num;
+        int sum = 0;
+        int product = 1;
+
+        while (num > 0) 
+        {
+            int digit = num % 10;
+            sum = sum + digit;
+            product = product * digit;
+            num = num/10;
+        }
+
+        if (sum == product) 
+        {
+            System.out.println(n + " is a Spy Number.");
+        } 
+        else 
+        {
+            System.out.println(n + " is not a Spy Number.");
+        }
+
+    }
+}
